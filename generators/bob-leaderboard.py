6cbfcf8f70c378657813b9f1670a20d7d04aca08
Desktop Folder <desktopdoesminecraft@gmail.com>
2021-04-05 00:01:26 -0700

    Add proper about/rules.

--- generators/bob-leaderboard.py
+++ generators/bob-leaderboard.py
@@ -1,6 +1,6 @@
 from .generator import Generator as MainGenerator
 
-runs_str = """<div class="run"><a href="{url}">{time} by {name}</a> {verified}</div>"""
+runs_str = """<div class="run">{num} <a href="{url}">{time} by {name}</a> {verified}</div>"""
 
 def to_secs(lst):
     t = lst[0]
@@ -15,4 +15,4 @@ def is_verified(i):
 class Generator(MainGenerator):
     def existing_runs(self):
         # Get all the existing runs. Chop chop!
-        return '\n'.join([runs_str.format(url=u, time=t, name=n, verified=is_verified(i)) for (t, u, n, i) in sorted([ln.strip().split(' ') for ln in open('data/bob-runs.txt', 'r').readlines() if len(ln) > 4], key=to_secs)])
+        return '\n'.join([runs_str.format(num=itr + 1, url=u, time=t, name=n, verified=is_verified(i)) for (itr, (t, u, n, i)) in enumerate(sorted([ln.strip().split(' ') for ln in open('data/bob-runs.txt', 'r').readlines() if len(ln) > 4], key=to_secs))])
