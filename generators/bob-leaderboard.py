f9e3b159e03831db96bd7a264db7eee62337616d
Desktop Folder <desktopdoesminecraft@gmail.com>
2021-04-05 11:34:49 -0700

    Make the page a bit more ... conformant.

--- generators/bob-leaderboard.py
+++ generators/bob-leaderboard.py
@@ -1,6 +1,6 @@
 from .generator import Generator as MainGenerator
 
-runs_str = """<div class="run">{num} <a href="{url}">{time} by {name}</a> {verified}</div>"""
+runs_str = """<tr class="run"><td>{num}</td><td><a href="{url}">{time}</a></td><td>{name}</td><td>{verified}</td></tr>"""
 
 def to_secs(lst):
     t = lst[0]
@@ -12,7 +12,12 @@ def is_verified(i):
     # so... lol
     return "Unverified"
 
+def urlify(n):
+    # does this name have an associated channel/page?
+    # let's just assume not...
+    return n
+
 class Generator(MainGenerator):
     def existing_runs(self):
         # Get all the existing runs. Chop chop!
-        return '\n'.join([runs_str.format(num=itr + 1, url=u, time=t, name=n, verified=is_verified(i)) for (itr, (t, u, n, i)) in enumerate(sorted([ln.strip().split(' ') for ln in open('data/bob-runs.txt', 'r').readlines() if len(ln) > 4], key=to_secs))])
+        return '\n'.join([runs_str.format(num=itr + 1, url=u, time=t, name=urlify(n), verified=is_verified(i)) for (itr, (t, u, n, i)) in enumerate(sorted([ln.strip().split(' ') for ln in open('data/bob-runs.txt', 'r').readlines() if len(ln) > 4], key=to_secs))])
