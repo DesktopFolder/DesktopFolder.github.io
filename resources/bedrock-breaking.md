9a32b1d0840633545514dfdff5b3520b10cef1d2
DesktopFolder <8965189+DesktopFolder@users.noreply.github.com>
2022-02-28 18:17:28 -0500

    Add resources subdirectory (currently empty).

---
+++ resources/bedrock-breaking.md
@@ -0,0 +1,8 @@
+Yeah, another filler page. It's fine. Nobody knows about this site anyways.
+
+You're probably here to break some bedrock, aren't you?
+
+### 1.13 - 1.14
+
+Currently optimal bedrock breaker (3-way):
+... I don't actually know, I never researched this. It's ok though.
--- templater.py
+++ templater.py
@@ -108,6 +108,7 @@ def files():
             'videos/piston-bedrock-breaking/explanation.dhtml',
             'videos/3way-users-guide/index.dhtml',
             'videos/3way-users-guide/guide.dhtml',
+            'resources/bedrock-breaking.dhtml',
             'tools/piston.dhtml',
             'tools/dc.dhtml',
             'discord/index.dhtml',
