9728b9419bee84af0c52769bc2a539d43ccff202
DesktopFolder <desktopfolder@users.noreply.github.com>
2025-04-18 14:06:35 -0400

    /draaft: Basic work...

--- js/draaft.ts
+++ js/draaft.ts
@@ -1,14 +1,18 @@
 import { pools } from "./draaft/options";
 
-let all_divs: string[] = []
-for (const pool of pools) {
-    console.log(`Adding pool: ${pool.prettyName}`)
-    all_divs.push(pool.makeDiv())
+function main() {
+    let all_divs: string[] = []
+    for (const pool of pools) {
+        console.log(`Adding pool: ${pool.prettyName}`)
+        all_divs.push(pool.makeDiv())
+    }
+
+    console.log("Creating full page.");
+    let full_data = all_divs.join('\n')
+
+    let container = document.createElement('div')
+    document.body.appendChild(container)
+    container.innerHTML = full_data
 }
 
-console.log("Creating full page.");
-let full_data = all_divs.join('\n')
-
-let container = document.createElement('div')
-document.body.appendChild(container)
-container.innerHTML = full_data
+document.addEventListener("DOMContentLoaded", main, false);
