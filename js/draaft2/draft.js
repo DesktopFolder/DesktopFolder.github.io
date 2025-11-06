e3765f702220a072797c425dccd3ad4bfb5dbbe1
DesktopFolder <8965189+DesktopFolder@users.noreply.github.com>
2025-11-05 19:02:18 -0500

    /draft: Start testing draft page layout

--- js/draaft2/draft.js
+++ js/draaft2/draft.js
@@ -17,6 +17,38 @@ export function startDrafting() {
     });
     apiRequest("draft/pick?key=bucket", undefined, "POST");
     displayOnlyPage("draft-page");
+    // Fill everything in, assuming we have it.
+    displayDraftables();
+}
+function displayDraftables() {
+    if (DRAFTABLES === undefined) {
+        setTimeout(displayDraftables, 200);
+        return;
+    }
+    let pools = DRAFTABLES[0];
+    let picks = DRAFTABLES[1];
+    for (const pool of pools) {
+        // let's not worry about preserving information about this stuff
+        let pd = document.createElement("div");
+        pd.id = `pool-${pool.name.short_name}`;
+        pd.classList.add("draft-pool-container", "draft-page-item");
+        for (const k of pool.contains) {
+            if (LOCAL_TESTING) {
+                console.log(picks[k]);
+            }
+            let pickd = document.createElement("div");
+            pickd.classList.add("draft-pick-container");
+            let name = document.createElement("p");
+            name.innerHTML = picks[k].name.short_name;
+            pickd.appendChild(name);
+            let desc = document.createElement("p");
+            desc.innerHTML = picks[k].description;
+            desc.classList.add("tooltip", "pick-tooltip");
+            pickd.appendChild(desc);
+            pd.appendChild(pickd);
+        }
+        document.getElementById('draft-page-main').appendChild(pd);
+    }
 }
 export let DRAFTABLES = undefined;
 export function fetchData() {
